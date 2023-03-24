de4acb4cc66093645607526d8bb5a50e95c64407
Chandan kumar <chandanpk9@gmail.com>
2023-03-24 20:21:06 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
-// export const BASE_URL = "https://my-json-server.typicode.com/ChandanPk/json-placeholder-api"
-export const BASE_URL = "https://tindog-sever.herokuapp.com"
+// export const BASE_URL =
+//   "https://my-json-server.typicode.com/ChandanPk/json-placeholder-api";
+// export const BASE_URL = "http://localhost:8001";
+export const BASE_URL = "https://tindog-api.onrender.com";
 
 export const API = {
-    blogs:  BASE_URL + "/blogs",
-}
+  blogs: BASE_URL + "/blogs",
+};
 
-API.blogPage = (id) => BASE_URL + "/blogs/" + id
+API.blogPage = (id) => BASE_URL + "/blogs/" + id;
